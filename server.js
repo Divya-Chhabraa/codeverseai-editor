edaefc15fd3123653d3673ba08b4b403dede49d3
Divya Chhabra <divichhabra49@gmail.com>
2025-11-20 20:26:16 +0530

    server upd

--- server.js
+++ server.js
@@ -356,5 +356,11 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
